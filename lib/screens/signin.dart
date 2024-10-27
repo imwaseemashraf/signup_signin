b1471a3d9352108c79f0587066c2f455e75b8278
imwaseemashraf <waseemashraf0201@gmail.com>
2024-10-27 07:42:31 +0500

    login complete

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -14,29 +14,30 @@ class SignIn extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             // mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const Align(
+            children: <Widget>[
+              const Gap(25),
+              Align(
                 alignment: Alignment.center,
                 child: Text(
-                  "Login",
-                  style: TextStyle(
+                  "Login".toUpperCase(),
+                  style: const TextStyle(
                     fontSize: 32,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
               const Spacer(),
-              _buildTextField("Enter Your Mobile Number", "+92 303-7498907",
+              _buildTextField("Enter Your Mobile Number", "+92 3001234567",
                   const Icon(Icons.check_circle)),
               const Gap(10),
               _buildTextField("Enter Your Password", "***********",
                   const Icon(CupertinoIcons.eye),
                   isPassword: true),
-              Gap(10),
+              const Gap(10),
               const Align(
                 alignment: Alignment.centerRight,
                 child: Text(
-                  "ForGet Password? ",
+                  "Forget Password? ",
                   style: TextStyle(
                     fontWeight: FontWeight.normal,
                   ),
@@ -44,7 +45,7 @@ class SignIn extends StatelessWidget {
               ),
               const Gap(15),
               Container(
-                height: 43,
+                height: 50,
                 width: double.infinity,
                 padding: const EdgeInsets.symmetric(horizontal: 10),
                 decoration: BoxDecoration(
@@ -63,6 +64,41 @@ class SignIn extends StatelessWidget {
                   ),
                 ),
               ),
+              const Gap(15),
+              RichText(
+                text: const TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "Don't Have An Account?",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, color: Colors.black),
+                      children: [
+                        TextSpan(
+                            text: " Sign Up",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 17)),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const Gap(5),
+              _buildOR("or"),
+              const Gap(15),
+              _buildContinueMethod(
+                  'assets/images/Google.png', "Continue With Google"),
+              const Gap(15),
+              _buildContinueMethod(
+                  'assets/images/Group.png', "Continue With Apple"),
+              const Gap(15),
+              _buildOR("or"),
+              const Text(
+                "Conitinue as Guest",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
+              ),
               const Spacer(),
             ],
           ),
@@ -76,10 +112,13 @@ class SignIn extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          label,
-          style: const TextStyle(
-            fontWeight: FontWeight.normal,
+        Padding(
+          padding: const EdgeInsets.only(left: 10.0),
+          child: Text(
+            label,
+            style: const TextStyle(
+              fontWeight: FontWeight.normal,
+            ),
           ),
         ),
         const Gap(10),
@@ -106,6 +145,35 @@ class SignIn extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+
+  Widget _buildOR(String text) {
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: const TextStyle(
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _buildContinueMethod(String imgUrl, String label) {
+    return Container(
+      height: 50,
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.grey[300],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(image: AssetImage(imgUrl)),
+          const Gap(10),
+          Text(label),
+        ],
+      ),
     );
   }
 }
